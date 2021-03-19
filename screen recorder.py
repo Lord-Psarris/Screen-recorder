55f7b8313ce22a987e59f4da808255479213cd8d
Lord-Psarris <theorchestra03@gmail.com>
2021-03-19 22:12:11 +0100

    Initial commit

---
+++ screen recorder.py
@@ -0,0 +1,44 @@
+import pyautogui
+import cv2
+import numpy as np
+
+print('started')
+
+resolution = (1920, 1080)
+
+_ = cv2.VideoWriter_fourcc(*'XVID')
+
+filename = "screen_record.avi"
+
+rate = 5
+
+# Creating a VideoWriter object
+out = cv2.VideoWriter(filename, _, rate, resolution)
+
+cv2.namedWindow("Screen recorder", cv2.WINDOW_NORMAL)
+
+# Resize this window
+cv2.resizeWindow("Screen recorder", 480, 270)
+
+while True:
+    # screenshot
+    img = pyautogui.screenshot()
+
+    # Convert the screenshot to a numpy array
+    frame = np.array(img)
+
+    # Convert to RGB
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame = cv2.resize(frame, (1920, 1080))
+
+    # Write it to the output file
+    out.write(frame)
+
+    # Optional: Display the screen
+    cv2.imshow('Screen recorder', frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+out.release()
+cv2.destroyAllWindows()
